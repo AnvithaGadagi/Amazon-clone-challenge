e1834ef33d187c3a4208c6d2031c8a830b36f7cd
Anvitha Gadagi <Anvitha@Shrishailas-MacBook-Pro.local>
2020-10-07 21:51:28 +0530

    Updates

---
+++ precache-manifest.f6ae0423ad9748f20e88121ce8f39bf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da49f21f2e66cbb4656cb91dc466959e",
+    "url": "/Amazon-clone-challenge/index.html"
+  },
+  {
+    "revision": "adcfb08d48776ec3571f",
+    "url": "/Amazon-clone-challenge/static/css/main.ab489b3c.chunk.css"
+  },
+  {
+    "revision": "0da4cd00268b60180e0c",
+    "url": "/Amazon-clone-challenge/static/js/2.a8fc2162.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/Amazon-clone-challenge/static/js/2.a8fc2162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adcfb08d48776ec3571f",
+    "url": "/Amazon-clone-challenge/static/js/main.6694dae8.chunk.js"
+  },
+  {
+    "revision": "57652c4f6ca0b4749251",
+    "url": "/Amazon-clone-challenge/static/js/runtime-main.0ea288eb.js"
+  }
+]);
